9482b0a0f39f74aa70cc3389a033520a40340fa7
Unknown <juliansubda@gmail.com>
2019-11-05 19:22:02 -0800

    updating routes

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -36,42 +36,39 @@ export class ApiService {
     this.isMS = window.navigator.msSaveOrOpenBlob ? true : false;
     const { hostname } = window.location;
     switch (hostname) {
-      case 'localhost':
-        // Local
-        this.apiPath = 'http://localhost:3000/api/public';
-        this.adminUrl = 'http://localhost:4200';
-        this.env = 'local';
-        break;
+  case 'localhost':
+    // Local
+    this.apiPath = 'http://localhost:3000/api/public';
+    this.adminUrl = 'http://localhost:4200';
+    this.env = 'local';
+    break;
 
-      case 'eagle-dev.pathfinder.gov.bc.ca':
-        // prod
-        this.apiPath = 'https://eagle-dev.pathfinder.gov.bc.ca/api/public';
-        this.adminUrl = 'https://eagle-dev.pathfinder.gov.bc.ca/admin/';
-        this.env = 'dev';
-        break;
+  case 'cap-eagle-dev.pathfinder.gov.bc.ca':
+    // prod
+    this.apiPath = 'https://cap-eagle-dev.pathfinder.gov.bc.ca/api/public';
+    this.adminUrl = 'https://cap-eagle-dev.pathfinder.gov.bc.ca/admin/';
+    this.env = 'dev';
+    break;
 
-      case 'www.test.projects.eao.gov.bc.ca':
-      case 'eagle-test.pathfinder.gov.bc.ca':
-      case 'test.projects.eao.gov.bc.ca':
-        // Test
-        this.apiPath = 'https://eagle-test.pathfinder.gov.bc.ca/api/public';
-        this.adminUrl = 'https://test.projects.eao.gov.bc.ca/admin/';
-        this.env = 'test';
-        break;
+  case 'cap-eagle-test.pathfinder.gov.bc.ca':
+    // Test
+    this.apiPath = 'https://cap-eagle-test.pathfinder.gov.bc.ca/api/public';
+    this.adminUrl = 'https://cap-eagle-test.pathfinder.gov.bc.ca/admin/';
+    this.env = 'test';
+    break;
 
-      case 'www.projects.eao.gov.bc.ca':
-      case 'projects.eao.gov.bc.ca':
-        // prod
-        this.apiPath = 'https://eagle-prod.pathfinder.gov.bc.ca/api/public';
-        this.adminUrl = 'https://projects.eao.gov.bc.ca/admin/';
-        this.env = 'prod';
-        break;
+  case 'cap-eagle-prod.pathfinder.gov.bc.ca':
+    // prod
+    this.apiPath = 'https://cap-eagle-prod.pathfinder.gov.bc.ca/api/public';
+    this.adminUrl = 'https://cap-eagle-prod.pathfinder.gov.bc.ca/admin/';
+    this.env = 'prod';
+    break;
 
-      default:
-        // Prod
-        this.apiPath = 'https://cap-minio-eagle-dev-azure-oabrei-dev.pathfinder.gov.bc.ca';
-        this.adminUrl = 'https://cap-minio-eagle-dev-azure-oabrei-dev.pathfinder.gov.bc.ca/admin/';
-        this.env = 'dev';
+  default:
+    // Prod
+    this.apiPath = 'https://cap-eagle-test.pathfinder.gov.bc.ca/api/public';
+    this.adminUrl = 'https://cap-eagle-test.pathfinder.gov.bc.ca/admin/';
+    this.env = 'test';
     };
   }
 
